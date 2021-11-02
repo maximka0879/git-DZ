5ca4142b7d73a57fbff3fdab3fd9a0f3dbf60d66
Max <kurs.maxim2017@yandex.ru>
2021-11-02 21:52:33 +0300

    ini project

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+$(document).ready(() => {
+    $('#form-btn').click(function () {
+        $('.form-error').hide();
+
+
+        let name = $('#form-name');
+        let address = $('#form-address');
+        let number = $('#form-number');
+
+        let hasError = false;
+        name.css('border-color', 'rgb(185, 145, 80)');
+        address.css('border-color', 'rgb(185, 145, 80)');
+        number.css('border-color', 'rgb(185, 145, 80)');
+
+
+        if (!name.val()) {
+            name.siblings('.form-error').show();
+            name.css('border-color', 'red');
+            hasError = true;
+        }
+        if (!address.val()) {
+            address.siblings('.form-error').show();
+            address.css('border-color', 'red');
+            hasError = true;
+        }
+        if (!number.val()) {
+            number.siblings('.form-error').show();
+            number.css('border-color', 'red');
+            hasError = true;
+        }
+        if (!hasError) {
+            let url = 'https://itlogia.ru/test/checkout?name=' + name.val() + '&address=' + address.val() + '&number=' + number.val();
+            console.log(url);
+            let loader = $('#loader');
+            loader.css('display', 'flex');
+
+
+
+            $.ajax({
+                method: "POST",
+                url: url,
+                data: { name: name.val(), address: address.val(), number: number.val()}
+            })
+                .done(function (message) {
+                    loader.hide();
+                    let result = message.success;
+                    let form = $('#order-form');
+                    let suc = $('#order-form-success')
+                    if (result) {
+                        form.css('display', 'none');
+                        suc.css('display', 'flex');
+
+                    } else {
+                        alert('Возникла ошибка при оформлении заказа, позвоните нам и сделайте заказ');
+
+                    }
+                });
+        }
+
+    });
+
+});
